1d974c386b1d5da0b2b05db4bc02eb5ca0cc1440
Richardy <yyc95102@vip.qq.com>
2018-02-14 09:30:00 +0800

    Add files via upload

---
+++ biliTest.py
@@ -0,0 +1,27 @@
+﻿import sys
+import ssl
+import urllib.request
+
+def report(count,blockSize,totalSize):
+	downloadedSize = count*blockSize
+	percent = int(downloadedSize*100/totalSize)
+	sys.stdout.write(f"\rDownloaded: {downloadedSize} bytes, Total: {totalSize} bytes, {percent} % complete")
+	sys.stdout.flush()
+
+if __name__ == '__main__':
+	ssl._create_default_https_context = ssl._create_unverified_context
+	opener = urllib.request.build_opener()
+	opener.addheaders = [
+        ('Host', 'cn-sdjn2-cmcc-v-12.acgvideo.com'),
+        ('User-Agent', 'XX'),
+        ('Accept', '*/*'),
+        ('Accept-Language', 'XX'),
+        ('Accept-Encoding', 'XX'),
+        ('Range', 'bytes=0-'), 
+        ('Referer', 'https://www.bilibili.com/video/av14694864/'),
+        ('Origin', 'https://www.bilibili.com'),
+        ('Connection', 'keep-alive'),
+	]
+	urllib.request.install_opener(opener)
+	url = 'https://cn-sdjn2-cmcc-v-12.acgvideo.com/vg5/upgcxcode/68/32/23953268/23953268-1-64.flv?XX'
+	urllib.request.urlretrieve(url, filename='av14694864.flv', reporthook=report)
